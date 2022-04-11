51476fd782362ed75b9e4cb8fec54480a1c94969
MuraYami <89479895+MuraYami@users.noreply.github.com>
2022-04-12 00:48:05 +0700

    Add files via upload

---
+++ Lab01/Ex63/Triangle.java
@@ -0,0 +1,21 @@
+package Ex63;
+import java.util.Scanner;
+
+public class Triangle {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Input the height of triangle n = ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++ ){
+            for (int j = 0; j < n - 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
