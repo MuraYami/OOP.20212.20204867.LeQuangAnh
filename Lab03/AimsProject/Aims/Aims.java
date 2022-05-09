c3639ec2ea74d8223ecf7be532af44db9f7041d8
MuraYami <89479895+MuraYami@users.noreply.github.com>
2022-05-09 21:13:10 +0700

    Add files via upload

--- Lab03/AimsProject/Aims/Aims.java
+++ Lab03/AimsProject/Aims/Aims.java
@@ -8,20 +8,20 @@ public class Aims {
         //Create new dvd objects and add them to the cart 
         DigitalVideoDisc dvd1  = new DigitalVideoDisc("The Lion King", 
                 "Animation", "Roger Allers", 87, 19.95f);
-        //anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
                 "Science Fiction", "George Lucas", 87, 24.95f);
-        //anOrder.addDigitalVideoDisc(dvd2);
+        anOrder.addDigitalVideoDisc(dvd2);
         
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", 
         "Animation", "abc", 25, 18.99f);
-        //anOrder.addDigitalVideoDisc(dvd3);
+        anOrder.addDigitalVideoDisc(dvd3);
         //anOrder.addDigitalVideoDisc(dvd2,dvd3);
-        dvdList[0] = dvd1;
-        dvdList[1] = dvd2;
-        dvdList[2] = dvd3;
-        anOrder.addDigitalVideoDisc(dvdList);
+        //dvdList[0] = dvd1;
+        //dvdList[1] = dvd2;
+        //dvdList[2] = dvd3;
+        //anOrder.addDigitalVideoDisc(dvdList);
 
         //print total cost of the items in cart
         System.out.print("Total Cost is: ");
